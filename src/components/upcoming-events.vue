74dc1b97dca20bf7b672c6943101fe81297607db
vaidikabharata <38800747+vaidikabharata@users.noreply.github.com>
2019-01-18 11:27:39 +0530

    Update upcoming-events.vue

--- src/components/upcoming-events.vue
+++ src/components/upcoming-events.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text_center" style="width: 372px">
         <h3>Upcoming Events</h3>
-        <p>Samaveda Kauthuma shakha - gurjara paddhati parayanam</p>
+       
         <p>Shukla yajurveda Madhyandina Ghana parayanam</p>
     </div>
 </template>
