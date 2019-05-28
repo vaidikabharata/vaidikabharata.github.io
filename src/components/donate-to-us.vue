72e650fcfe42a2e0ab68d1bf0bd72dfab96bc363
vaidikabharata <akarshan3.0@gmail.com>
2019-05-28 18:34:46 -0400

    Add instamojo link

--- src/components/donate-to-us.vue
+++ src/components/donate-to-us.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="text_center"  style="width: 215px">
+    <div class="text_center"  style="width: 315px">
         <h2>Donate to us:</h2>
+        <br/>
+        <p><a href="https://www.instamojo.com/@vaidikabharatatrust" target="_blank">
+            Click here to donate through instamojo.</a></p>
+        <br/>
+        <h4>Bank details for direct payment:</h4>
         <p><b>Bank:</b> ICICI Bank</p>
         <p><b>Account Number:</b> 073405001350</p>
         <p><b>Account Type:</b> Current</p>
         <p><b>Account Name:</b> Vaidik Bharat Trust</p>
-        <p><b>IFSC:</b> icic0000734</p>
+        <p><b>IFSC:</b> ICIC0000734</p>
         <p><b>Address:</b></p>
         <p>Agra Chowk, Plot No. 5. New Colony.</p>
         <p>Palwal, Haryana.</p>
         <p>P.I.N: 121102</p>
+        <br/>
         <p><b>PayTM QR Code</b></p>
-        <img class="qr-img" src='./img/payTMQRCode.jpg'>
+        <img alt="payTM QR Code" class="qr-img" src='./img/payTMQRCode.jpg'>
     </div>
 </template>
 
